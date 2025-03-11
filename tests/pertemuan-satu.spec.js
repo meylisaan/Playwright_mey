b40b3cb97371ff0e7401ca341fa0655452ac6610
meylisaan <meylisanditya@gmail.com>
2025-03-11 23:17:16 +0700

    push login

--- tests/pertemuan-satu.spec.js
+++ tests/pertemuan-satu.spec.js
@@ -2,7 +2,25 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
+  const inpUser ='standard_user';
+  const inpPass ='secret_sauce';
+
+  //Input Username
+  const inputUsername = page.locator('#user-name');
+  await inputUsername.fill(inpUser);
+  await expect(inputUsername).toHaveValue(inpUser);
+
+  //Input Password
+  const inputPass = page.locator('#password');
+  await inputPass.fill(inpPass);
+  await expect(inputPass).toHaveValue(inpPass);
+
+  //Login
+  const ctaLogin = page.locator('#login-button');
+  await ctaLogin.click();
+
+
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Swag Labs');
+  //await expect(page).toHaveTitle('Swag Labs');
 });
